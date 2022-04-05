9806ac8783ba04d68ff53f81e5c17db926261473
wildfirez98 <james@areafamily.us>
2022-04-05 18:01:53 -0500

    First commit, testing with Snapshot complete

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+        <h3>Testing Snapshots Out!</h3>
+        <h2>Another Update!</h2>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('Renders a snapshot', () => {
+  const tree = renderer.create(<App />).toJSON()
+  console.log(tree)
+  expect(tree).toMatchSnapshot()
+})
